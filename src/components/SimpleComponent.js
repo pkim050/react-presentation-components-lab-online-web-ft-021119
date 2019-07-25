1480c277ddeca444f211e04802ba99420a1a8cda
Patrick Kim <pkim050@gmail.com>
2019-07-25 10:03:47 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,16 @@
 // Code SimpleComponent Here
+import React from 'react'
+
+class SimpleComponent extends React.Component {
+  state = {
+    mood: "happy"
+  }
+
+  render() {
+    return(
+      <div>
+        <p>{this.state.mood}</p>
+      </div>
+    )
+  }
+}
